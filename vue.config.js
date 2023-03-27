fdd70adaab36a5d16b6cff7daa0df526b0cc095b
shengge520 <a1656667614@gmail.com>
2023-03-27 09:41:16 +0800

    update port

--- vue.config.js
+++ vue.config.js
@@ -18,7 +18,7 @@ module.exports = {
       port: 8080,
       https: false,
       hotOnly: false,
-      open: true, //配置自动启动浏览器
+      open: false, //配置自动启动浏览器
       proxy: {// 配置多个代理(配置一个 proxy: 'http://localhost:4000' )
         '/api': {
           target: '<url>',
