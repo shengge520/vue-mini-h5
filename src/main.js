7bea077b2a0bf7b89c10bf71d63ffa014904dde4
shengge520 <a1656667614@gmail.com>
2023-03-31 15:36:39 +0800

    add Vant

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
-import { createApp } from 'vue'
+import Vue, { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router"
 // 导入vant
 import Vant from "vant";
 import "vant/lib/index.css";
 
+Vue.use(Vant);
+
 const app = createApp(App)
 
-Vue.use(Vant);
+
 
 app.use(router)
 app.mount('#app')
